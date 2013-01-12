73eac68ed82853502bbce6dcb9be923fd0bcbacb
Dustin Zeisler <Dustin@Zeisler.net>
2013-01-12 13:24:59 -0800

    Simplified Code

--- Base.class.rb
+++ Base.class.rb
@@ -1,113 +1,78 @@
 class Base
-	attr_accessor :select, :selectValue, :update, :updateValue, :insert, :insertValue,
-					:set, :setValue, :from, :fromValue, :where, :whereValue
-	attr_writer :statment
-	attr_writer :type
+	def hashItOut(hash, num)
+		@type_ = ""
+		if hash.is_a?(Hash)
+			hash.each { |key, value|
+		   		if num >= 1 then @type_ += ", `#{key}` = '#{value}'"
+				else @type_ += "`#{key}` = '#{value}'"
+				end
+				num = num + 1
+			}
+		else
+			if num >= 1 then @type_ += ", `#{hash}`"
+			else @type_ += "`#{hash}`"
+			end
+			num = num + 1
+		end
+		num
+	end
 	def select(select)
 		@type = "SELECT"
-
-		if @selectValue == nil
-			@selectValue = 0
-		end
-		if @select == nil
-			@select ="SELECT "
-		end
-		if (@selectValue >= 1)
-			@select += ", `#{select}`"
-		else
-			@select += "`#{select}`"
-		end
-		@selectValue = @selectValue + 1
+		@selectValue ||= 0
+		@select ||= "SELECT "
+		@selectValue = hashItOut(select,@selectValue)
+		@select += @type_
 	end
 
 	def update(update)
 		@type = "UPDATE"
-		if @updateValue == nil
-			@updateValue = 0
-		end
-		if @update == nil
-			@update ="UPDATE  "
-		end
-		if (@updateValue >= 1)
-			@update += ", `#{update}`"
-		else
-			@update += "`#{update}`"
-		end
-		@updateValue = @updateValue + 1
+		@updateValue ||= 0
+		@update ||="UPDATE  "
+		@updateValue = hashItOut(update,@updateValue)
+		@update  += @type_
 	end
 
 	def insert(to)
 		@type = "INSERT"
-		if @insert == nil
-			@insert ="INSERT INTO #{to}"
-		end
+		@insert = "INSERT INTO #{to}"
 	end
 
 	def set(set = Hash{})
-		if @setValue == nil
-			@setValue = 0
-		end
-		if @set == nil
-			@set ="SET "
-		end
-		set.each { |key, value|
-   		if (@setValue >= 1)
-			@set += ", `#{key}` = '#{value}'"
-		else
-			@set += "`#{key}` = '#{value}'"
-		end
-		@setValue = @setValue + 1
-		}
-
+		@setValue ||= 0
+		@set ||="SET "
+		@setValue = hashItOut(set,@setValue)
+		@set  += @type_
 	end
 
 	def from(from)
-		if @fromValue == nil
-			@fromValue = 0
-		end
-		if @from == nil
-			@from =" FROM "
-		end
-		if (@fromValue >= 1)
-			@from += ", `#{from}`"
-		else
-			@from += "`#{from}`"
-		end
-		@fromValue = @fromValue + 1
+		@fromValue ||= 0
+		@from ||=" FROM "
+
+		@fromValue = hashItOut(from,@fromValue)
+		@from  += @type_
 	end
 
 	def where(where = Hash{})
-		if @whereValue == nil
-			@whereValue = 0
-		end
-		if @where == nil
-			@where =" WHERE "
-		end
+		@whereValue ||= 0
+		@where ||=" WHERE "
 		condition = where['condition']
 		where.delete('condition')
 		where.each { |key, value|
-   		if (@whereValue >= 1)
-			@where += " #{condition} `#{key}` = '#{value}'"
-		else
-			@where += " `#{key}` = '#{value}'"
-		end
-		@whereValue = @whereValue + 1
+	   		if @whereValue >= 1 then @where += " #{condition} `#{key}` = '#{value}'"
+			else @where += " `#{key}` = '#{value}'"
+			end
+			@whereValue = @whereValue + 1
 		}
-
 
 	end
 	def statement
-		if @type == "UPDATE"
-			@statment = "#{@update} #{@set} #{@where}"
-		elsif @type == "SELECT"
-			@statment = "#{@select} #{@from} #{@where}"
-		elsif @type == "INSERT"
-			@statment = "#{@insert} #{@set} #{@where}"
+		if @type == "UPDATE" then @statment = "#{@update} #{@set} #{@where}"
+		elsif @type == "SELECT" then @statment = "#{@select} #{@from} #{@where}"
+		elsif @type == "INSERT" then @statment = "#{@insert} #{@set} #{@where}"
 		end
 	end
 
 	def print
-
 		statement
 		puts "#{@statment}"
 	end
